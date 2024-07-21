3be3610d6b2118da3436d3cf8a950bea28857cbd
Kudos <kudosbeluga@gmail.com>
2024-07-20 19:30:37 -0500

    bruv

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(express.json({ limit: '100mb' }));
 app.use(cors())
 app.post('/upload', (req, res) => {
     const { uri } = req.body;
+    console.log(uri)
     if (!uri) return res.status(400).send({ error: 'No URI provided' });
     require("fs").writeFile("out.png", uri.replace(/^data:image\/png;base64,/, ""), 'base64', async function(err) {    
         const result = await model.generateContent([
-        "I want you to send back a JSON object only, not even formatting. It must have a recyclable key, the value of which is a boolean that says whether the object in focus is recyclable or not. There should also be a type key, the value of which is a string, saying the following: \"This is a or these are [type of object in focus], which is/are\". Lastly, there should be an info key, the value of which is a string. This value should have any additional information or facts about its eco friendliness",
+        "I want you to send back a JSON object only, not even formatting. It must have a recyclable key, the value of which is a boolean that says whether the object in focus is recyclable or not. There should also be a type key, the value of which is a string, saying the following: \"This is a or these are [type of object in focus], which is/are    \". Lastly, there should be an info key, the value of which is a string. This value should have any additional information or facts about its eco friendliness",
         {
             inlineData: {
               data: uri.replace(/^data:image\/png;base64,/, ""),
